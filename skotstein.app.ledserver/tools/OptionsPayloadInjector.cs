74528ed04f965115660d0ee664c1dcc0e021434b
Sebastian Kotstein <sebastian.kotstein@reutlingen-university.de>
2019-08-12 22:01:46 +0200

    Remove test message from HTTP Options response

--- skotstein.app.ledserver/tools/OptionsPayloadInjector.cs
+++ skotstein.app.ledserver/tools/OptionsPayloadInjector.cs
@@ -17,11 +17,13 @@ namespace skotstein.app.ledserver.tools
 
         public override void Manipulate(RoutedContext context)
         {
+            /*
             if(context.Context.Request.Method == HttpMethod.OPTIONS)
             {
-                context.Context.Response.Payload.Write("YAML Description comes here");
+                context.Context.Response.Payload.Write("");
                 context.Context.Response.Headers.Set("Content-Type", MimeType.APPLICATION_JSON);
             }
+            */
             
         }
     }
